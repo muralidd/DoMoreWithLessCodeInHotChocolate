bfcc4021f3e1b7bff72a5b19d6325c3f8ad47de8
Muralidharan <muralid@ramco.com>
2022-01-24 23:06:08 +0530

    added schema file

---
+++ Example/src/Graphql/Custom/UseMySorting.cs
@@ -0,0 +1,26 @@
+﻿using HotChocolate.Types;
+using HotChocolate.Types.Descriptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Graphql.Custom
+{
+    public class UseMySortingAttribute : ObjectFieldDescriptorAttribute
+    {
+        public UseMySortingAttribute([CallerLineNumber] int order = 0)
+        {
+            Order = order;
+        }
+
+        public override void OnConfigure(IDescriptorContext context
+            , IObjectFieldDescriptor descriptor
+            , MemberInfo member)
+        {
+            descriptor.UseSorting();
+        }
+    }
+}
--- Example/src/Graphql/Startup.cs
+++ Example/src/Graphql/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Example.DataAccess;
 using HotChocolate.Types.Relay;
+using System;
+using System.Reflection;
 
 namespace Example.Graphql
 {
@@ -20,9 +22,13 @@ namespace Example.Graphql
 
         public void ConfigureServices(IServiceCollection services)
         {
+            String schemaPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) , @"gqlschema");
+            String schemaFile = String.Format(@"{0}\schema.graphql", schemaPath);
+
             services.AddSingleton<IUserRepository, UserRepository>();
             services.AddSingleton<IBookingsRepository, BookingsRepository>();
             services.AddGraphQLServer()
+                .AddDocumentFromFile(schemaFile)
                 .AddQueryType<Query>()
                 .AddMutationType<Mutation>()
                 .AddType<UserTypeExtension>()
